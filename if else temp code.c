288578cf2135941bab4c9398e0fa927c74ab7088
khadseashish2006-sketch <khadseashish2006@gmail.com>
2025-09-12 02:59:30 -0700

    Create if else temp code.c
    detect temp of the patient and tell wheter the patient is normal ,critical ,or warninhg

---
+++ if else temp code.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+
+void main() 
+{
+    float threshold=80,critical=100,sensor_temp ;
+     printf("Enter patient body temp:\n");
+     scanf("%f",&sensor_temp);
+{
+    if (sensor_temp<=threshold)
+        {
+         printf("Patient condition:NORMAL\n");
+        }
+     
+    else if (sensor_temp >threshold && sensor_temp<critical)
+        {
+         printf("Patient condition:WARNING\n");
+        }
+
+    else
+         {
+         printf("Patient condition:CRITICAL\n");
+         }
+    } 
+    
+  
