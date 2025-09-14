acdce7356ec27fffa3f278b7ba732ca98064b767
khadseashish2006-sketch <khadseashish2006@gmail.com>
2025-09-14 12:38:15 +0000

    enter A/B to run

---
+++ temp div.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+void main()
+{
+float A,B,div;
+
+printf("enter Temp A and B:");
+scanf("%f/%f",&A,&B);
+
+
+div=A/B;
+
+printf("The division of temp A and B is:%2f\n",div);
+
+}
