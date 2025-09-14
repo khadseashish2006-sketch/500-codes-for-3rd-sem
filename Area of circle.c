62ef28f5a11cedc5e2f85e90f134e85e734daf8d
khadseashish2006-sketch <khadseashish2006@gmail.com>
2025-09-14 14:02:28 +0000

    Implement area calculation for a circle

--- Area of circle.c
+++ Area of circle.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
+#define PI 3.141592653589793
 
+int main() {
+    double radius, area;
+
+    printf("Enter the radius of the circle: ");
+    scanf("%lf", &radius);  // %lf for double
+
+    area = PI * radius * radius;  // Area formula
+    printf("The area of the circle is: %.6lf\n", area);
+
+    return 0;
+}
+// This program calculates the area of a circle given its radius.
